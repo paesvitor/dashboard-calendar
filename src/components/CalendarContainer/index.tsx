2d690e0adb4937a2435cb2d549e26fbd49330988
Vitor Paes <paesvitor@outlook.com>
2020-11-18 11:37:34 -0300

    8 pilha eh 1 real

--- src/components/CalendarContainer/index.tsx
+++ src/components/CalendarContainer/index.tsx
@@ -1,8 +1,21 @@
-import moment, { Moment } from 'moment'
-import React, { useEffect, useState } from 'react';
-import { number } from 'yargs';
+import moment from 'moment'
+import React, { useState } from 'react';
 import { useStyles } from './styles';
-import clsx from 'clsx'
+import clsx from 'clsx';
+
+const events = [
+    {
+        start: '2020-03-20',
+        end: '2020-04-03',
+        id: 389
+    },
+
+    {
+        start: '2020-01-31',
+        end: '2020-02-06',
+        id: 1
+    }
+]
 
 function CalendarContainer() {
     //State
@@ -10,7 +23,7 @@ function CalendarContainer() {
     // const [blankElements, setBlankElements] = useState<(Element | JSX.Element)[]>([])
     // const [daysInMonthElements, setDaysInMonthElements] = useState<(Element | JSX.Element)[]>([]);
     // const [totalSlots, setTotalSlots] = useState<(Element | JSX.Element)[]>([]);
-    // const [rows, setRows] = useState([]);
+    const [calendarWidth, setCalendarWidth] = useState(0);
 
     // Constants
     const weekdayshort = moment.weekdaysShort();
@@ -40,20 +53,53 @@ function CalendarContainer() {
         let arr = []
 
         for (let i = 0; i < firstDayOfMonth(month); i++) {
-            const element = <td className={clsx(classes.day, classes.emptyDay)}>{""}</td>
+            const element = <td className={clsx(classes.dayWrapper, classes.emptyDay)}>{""}</td>
             arr.push(element)
         }
         return arr;
     }
 
     function fillDaysInMonth(month: number) {
-        const dateObject = moment().set('month', month);
+        const dateObject = moment().set('month', month).set('year', 2020);
 
         let arr = [];
 
         for (let d = 1; d <= dateObject.daysInMonth(); d++) {
-            const element = <td key={d} className={classes.day}>
-                {d}
+            let dayHasEventStart = false;
+            let dayHasEventEnd = false;
+            let eventEndDate = '';
+            let tempEvent: any = null;
+            const dayFormated = dateObject.set('date', d).format('YYYY-MM-DD');
+
+            events.map(event => {
+                if (event.start === dayFormated) {
+                    dayHasEventStart = true;
+                    eventEndDate = event.end;
+                    tempEvent = event;
+                } else if (event.end === dayFormated) {
+                    dayHasEventEnd = true
+                }
+            });
+
+            const element = <td
+                onClick={() => handleClickDate(dayFormated)}
+                data-is-startdate={dayHasEventStart}
+                data-is-enddate={dayHasEventEnd}
+                key={dayFormated} id={dayFormated}
+                className={clsx(classes.dayWrapper, dayHasEventStart && classes.dayHasEventStart, dayHasEventEnd && classes.dayHasEventEnd)}
+            >
+                <div className={classes.day} >
+                    {d}
+                </div>
+
+                {dayHasEventStart &&
+                    <div
+                        data-eventid={tempEvent.id}
+                        data-eventend={eventEndDate}
+                        className={classes.eventTimeline}
+                        onClick={(e) => handleClickEvent(e, tempEvent)}>
+                        Booking
+                </div>}
             </td>
             arr.push(element);
         }
@@ -64,29 +110,62 @@ function CalendarContainer() {
     function getRows(month: number): [] {
         const totalSlots = [...fillBlankSpaces(month), ...fillDaysInMonth(month)];
 
-        let tempCells: any = [];
-        let tempRows: any = [];
+        let cells: any = [];
+        let rows: any = [];
 
         totalSlots.forEach((row, i) => {
             if (i % 7 !== 0) {
-                tempCells.push(row); // if index not equal 7 that means not go to next week
+                cells.push(row); // if index not equal 7 that means not go to next week
             } else {
-                tempRows.push(tempCells); // when reach next week we contain all td in last week to rows 
-                tempCells = []; // empty container 
-                tempCells.push(row); // in current loop we still push current row to new container
+                rows.push(cells); // when reach next week we contain all td in last week to rows 
+                cells = []; // empty container 
+                cells.push(row); // in current loop we still push current row to new container
             }
             if (i === totalSlots.length - 1) { // when end loop we add remain date
-                tempRows.push(tempCells);
+                rows.push(cells);
             }
         });
-        return tempRows
+        return rows
     }
 
+    // function paintRows() {
+    //     getRows().map(row => console.log(row))
+    // }
+
     function renderDaysInMonth(month: number) {
-        return getRows(month).map((d: any, i: any) => {
-            return <tr>{d}</tr>;
+        return getRows(month).map((row: (Element | JSX.Element)[], i: any) => {
+            if (month === 2) {
+                // Check each row individually
+                row.map((el: any) => {
+                    // console.log(el.props)
+                })
+            }
+
+
+            return <tr className="calendar-line">{row}</tr>;
         });
     }
+
+    function calculateCalendarWidth() {
+        const rootElement = document.getElementById('root');
+        if (rootElement) {
+            setCalendarWidth(rootElement?.offsetWidth)
+        }
+    }
+
+    function handleClickDate(date: any) {
+        console.log('Clicked on date - ', date)
+    }
+
+    function handleClickEvent(event: React.MouseEvent, date: any) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log(date)
+    }
+
+    // useEffect(() => {
+    //     console.log('Clicked on date', calendarWidth)
+    // }, [calendarWidth])
 
     // useEffect(() => {
     //     setTotalSlots([...blankElements, ...daysInMonthElements])
@@ -97,8 +176,8 @@ function CalendarContainer() {
     // }, [totalSlots])
 
     return (
-        <div className={classes.root}>
-            <table className={classes.table}>
+        <div className={classes.root} id="root">
+            <table className={classes.weekDays}>
                 <thead>
                     <tr>
                         {weekdayshortname}
@@ -107,12 +186,12 @@ function CalendarContainer() {
             </table>
 
             <section className={classes.monthList}>
-                {monthsInYear.map((month, index) => <section className={classes.month}>
-                    <h2>{month}</h2>
+                {monthsInYear.map((month, index) => <section key={month} className={classes.month}>
+                    <h2 className={classes.monthName}>{month}</h2>
 
                     <table className={classes.table}>
                         <tbody>
-                            {renderDaysInMonth(index + 1)}
+                            {renderDaysInMonth(index)}
                         </tbody>
                     </table>
                 </section>)}
--- src/components/CalendarContainer/styles.ts
+++ src/components/CalendarContainer/styles.ts
@@ -2,11 +2,23 @@ import { makeStyles } from '@material-ui/styles';
 
 export const useStyles = makeStyles({
   root: {
+    position: 'relative'
+  },
 
+  weekDays: {
+    width: '100%',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    backgroundColor: 'lightgray',
+    borderBottom: '1px solid',
+    zIndex: 2
   },
 
   weekDay: {
-    textAlign: 'left'
+    textAlign: 'left',
+    width: '14.28571428571429%',
+    height: 60
   },
 
   table: {
@@ -14,20 +26,63 @@ export const useStyles = makeStyles({
   },
 
   monthList: {
-    width: '100%'
+    width: '100%',
+    marginTop: 120
   },
 
   month: {
-    width: '100%'
+    width: '100%',
+    marginBottom: 30
   },
 
-  day: {
+  monthName: {
+    textAlign: 'center',
+    marginBottom: 30
+  },
+
+  dayWrapper: {
       height: 90,
       border: '1px solid',
-      width: '14.28571428571429%'
+      width: '14.28571428571429%',
+      flex: 'none',
+      position: 'relative',
+      cursor: 'pointer',
+
+      '&:hover': {
+        backgroundColor: 'mistyrose'
+      }
+  },
+  
+  day: {
+    position: 'absolute',
+    top: 3,
+    right: 3
+  },
+
+  eventTimeline: {
+    position: 'absolute',
+    left: 0,
+    backgroundColor: 'green',
+    padding: 5,
+    cursor: 'pointer',
+    borderRadius: 3,
+    zIndex: 3
+  },
+
+  dayHasEventStart: {
+    backgroundColor: 'red'
+  },
+
+  dayHasEventEnd: {
+    backgroundColor: 'blue'
   },
 
   emptyDay: {
-      border: 'none'
+      border: 'none',
+
+      '&:hover': {
+        backgroundColor: 'unset',
+        cursor: 'unset'
+      }
   }
 });
